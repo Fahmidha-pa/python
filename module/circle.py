a897a2178c6bf7ebf6c68a02acee1e10ab991db8
Fahmidha-pa <116252309+Fahmidha-pa@users.noreply.github.com>
2023-01-18 16:01:21 +0530

    Add files via upload

---
+++ module/circle.py
@@ -0,0 +1,4 @@
+def circlearea(r):
+    print("Area of circle:",3.14*r*r)
+def circleperi(r):
+    print("perimeter of circle:",2*3.14*r)
---
+++ module/rect.py
@@ -0,0 +1,4 @@
+def rectarea(l,b):
+    print("Area of rectangle:",l*b)
+def rectperi(l,b):
+    print("perimeter of rectangle:",2*(l+b))
